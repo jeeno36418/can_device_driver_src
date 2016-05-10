d513f89392a99750bb873d23f3d60ae1ae843c32
jeeno <jeeno.736418@gmail.com>
2016-05-10 22:56:51 +0530

    calculator

---
+++ c_program basic/calculator.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void main()
+{
+	float a,b;
+	char operator,flag=1;
+	
+	while(flag)
+	{
+		
+		
+		printf("\nEnter two numbers\n");
+		scanf("%f%f",&a,&b);
+	 	    
+		printf("\nSelect opertaion +,-,/,*\n");
+        scanf("%s",&operator);
+        
+	
+		switch(operator)
+		{
+		
+			case '+':
+				    a=a+b;
+				    printf("\n%f",a);
+				    break;
+			case '-':
+				    a=a-b;
+				    printf("\n%f",a);
+					break;		    
+			case '/':
+				    a=a/b;
+				    printf("\n%f",a);
+				    break;
+			case '*':
+				    a=a*b;
+				    printf("\n%f",a);
+				    break;
+			default:
+					printf("invalid operation %d",operator);
+					
+	    }
+		printf("\nDo you want to continue press y or n\n");
+		scanf("%s",&flag);
+		if(flag=='y'||flag=='Y')
+		flag=1;
+		else
+		flag=0;
+		
+		
+	}
+	
+}
